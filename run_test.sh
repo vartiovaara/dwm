679c54bf3258fb9c6f74c06e67a553e54e1bedde
Pyry Vartiovaara <pyry.vartiovaara@protonmail.com>
2024-03-21 00:03:27 +0200

    Added background to testing script

--- run_test.sh
+++ run_test.sh
@@ -4,4 +4,6 @@ DISPLAY_XEPHYR=:30
 
 Xephyr $DISPLAY_XEPHYR -screen 1024x768 &
 sleep 1
-DISPLAY=$DISPLAY_XEPHYR ./dwm
+DISPLAY=$DISPLAY_XEPHYR ./dwm &
+sleep 1
+DISPLAY=$DISPLAY_XEPHYR feh --bg-scale $HOME/.local/share/backgrounds/bg.jpg
